a6a54884ca0628e4e7bcbb8336f64d88b49113f3
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-07-13 17:54:00 +0530

    programming

---
+++ C program/areaofrectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int length,breadth,area;
+	printf("length:");
+	scanf("%d",&length);
+	printf("breadth:");
+	scanf("%d",&breadth);
+	area=length*breadth;
+	printf("area=%d",area);
+	
+}
---
+++ C program/marksheet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int sub1,sub2,sub3,result;
+	printf("enter sub1 marks:");
+	scanf("%d",&sub1);
+	printf("enter sub2 marks:");
+	scanf("%d",&sub2);
+	printf("enter sub3 marks:");
+	scanf("%d",&sub3);
+	result=sub1+sub2+sub3;
+	printf("Result=%d",result);
+	
+	}
