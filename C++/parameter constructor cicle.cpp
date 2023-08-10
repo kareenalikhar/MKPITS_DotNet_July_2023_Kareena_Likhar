e548ad174676427c00f0b6581d01bf6264c8eaaa
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-08-10 20:54:59 +0530

    programming

---
+++ C++/parameter constructor cicle.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class circle
+{
+	public:
+	int radius;
+cirle(int radius)
+{
+	radius=radius;
+}
+float calculatearea()
+{
+	float a=3.14f*radius*radius;
+	return a;
+}
+float calculatecoc()
+{
+float coc=2*3.14f*radius;
+return coc;
+}
+};
+int main()
+{
+	float result,radius;
+	circle c1;
+	cout<<"Enter radius:";
+	cin>>c1.radius;
+	result=c1.calculatearea();
+	cout<<"Area="<<result<<endl;
+	result=c1.calculatecoc();
+	cout<<"circumference of circle:"<<result<<endl;
+	return 0;
+}
