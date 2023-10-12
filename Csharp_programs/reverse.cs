1674ed49b712aba9631042c5b6009fa368f587a4
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-10-12 23:59:12 +0530

    csharp

---
+++ Csharp_programs/reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace reverse
+{
+    internal class Reverse
+    {
+        static void Main(string[] args)
+        {
+            char letter1, letter2, letter3;
+
+            Console.Write("Enter letter1 :");
+            letter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter letter2 :");
+            letter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter letter3 :");
+            letter3 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write(" " + letter3);
+            Console.Write(" " + letter2);
+            Console.Write(" " + letter1);
+
+            Console.ReadKey();
+        }
+    }
+}
