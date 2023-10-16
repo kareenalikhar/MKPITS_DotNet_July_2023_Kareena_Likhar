f7b303964af5b60572ab60ef4f32bf29867415c8
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-10-16 17:14:42 +0530

    csharp

---
+++ Csharp_programs/equations2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equations2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            
+            Console.WriteLine("Enter value of Y: ");
+            y=Convert.ToInt32(Console.ReadLine());
+
+            x = (y * y) + 2 * y + 1;
+            Console.WriteLine("Value of X: " + x);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Csharp_programs/operators1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+            char op;
+            Console.WriteLine("Enter num1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num2: ");
+            num2=Convert.ToInt32(Console.ReadLine());
+
+            result = num1 + num2;
+            Console.WriteLine("addition: " + result);
+            result = num1 - num2;
+            Console.WriteLine("Substraction: " + result);
+            result = num1 * num2;
+            Console.WriteLine("Substraction: " + result);
+            result = num1 / num2;
+            Console.WriteLine("Substraction: " + result);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
