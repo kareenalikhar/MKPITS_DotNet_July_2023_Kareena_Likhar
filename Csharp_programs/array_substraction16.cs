1591600fbd72df0019a5f45f5015f751755184d3
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-10-29 15:54:09 +0530

    array

---
+++ Csharp_programs/array_substraction16.cs
@@ -0,0 +1,49 @@
+﻿//16. Write a C# Sharp program for the subtraction of two matrixes.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_substraction16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[2, 2];
+            int[,] arr2 = new int[2, 2];
+            int[,] arr3 = new int[2, 2];
+
+            Console.WriteLine("Enter 1st matrix: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Enter 2nd matrix: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nAfter Substraction");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr3[i, j] = arr1[i, j] - arr2[i, j];
+                    Console.Write(arr3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp_programs/array_transpose18.cs
@@ -0,0 +1,49 @@
+﻿//18. Write a C# Sharp program to find the transpose of a given matrix.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_transpose18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[2, 2];
+            int[,] arr3 = new int[2, 2];
+
+            Console.WriteLine("Enter 1st matrix: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Transpose the matrix
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr3[j, i] = arr1[i, j];
+                }
+            }
+
+            Console.WriteLine("Transposed matrix: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
