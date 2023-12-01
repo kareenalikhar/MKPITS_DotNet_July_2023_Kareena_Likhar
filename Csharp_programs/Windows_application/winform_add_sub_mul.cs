fa08a52ab7c4ffc0dab00fae511082eab4366bfb
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-12-01 18:12:12 +0530

    windows form

---
+++ Csharp_programs/Windows_application/winform_add_sub_mul.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_add_sub_mul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Calculation c=new Calculation();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.Add(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label3.Text = c.display();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.Sub(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label3.Text = c.display();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.Mul(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label3.Text = c.display();
+        }
+    }
+}
---
+++ Csharp_programs/Windows_application/winform_salary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_salary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Salary s=new Salary();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.sal(textBox1.Text, Convert.ToInt32(textBox2.Text), textBox3.Text);
+            label4.Text=s.display();
+        }
+    }
+}
