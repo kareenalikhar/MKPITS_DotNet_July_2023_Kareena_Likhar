145ac9fb7edfd79638f2e24d977ace69f9223a2e
Kareena Likhar <kareenalikhar2002@gmail.com>
2023-12-09 16:58:12 +0530

    console

---
+++ Csharp_programs/machine_test_Animal.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace machine_test_Animal
+{
+    class Animal
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal make sound");
+        }
+        public void display()
+        {
+
+        }
+    }
+
+    class Mammal : Animal
+    {
+        public string FurColor { get; set; }
+
+        public Mammal(string name, int age, string FurColor):base(name, age) 
+        {
+            this.FurColor = FurColor;
+        }
+        public void walk()
+        {
+            Console.WriteLine("Mammal is walking");
+        }
+    }
+    class Reptile:Animal
+    {
+        public bool Iscoldblooded { get; set; }
+
+        public Reptile(string name, int age, bool Iscoldblooded) : base(name, age)
+        {
+            this.Iscoldblooded = Iscoldblooded;
+        }
+        public void Crawl()
+        {
+            Console.WriteLine("Reptile is Crawling");
+        }
+    }
+
+    class Bird:Animal
+    {
+        public int wing { get; set; }
+
+        public Bird(string name, int age, int wing) : base(name, age)
+        {
+            this.wing = wing;
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+    }
+
+    class Lion:Mammal
+    {
+        public Lion(string name, int age, string furColor) : base(name, age, furColor) 
+        { 
+        
+        }
+        public void makesound()
+        {
+            Console.WriteLine("The Lion Roars Loaudly");
+        }
+        public void display()
+        {
+            Console.WriteLine("name: "+name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Furcolor: " + FurColor);
+        }
+
+    }
+    class Snake:Reptile
+    {
+        public Snake(string name, int age, bool Iscoldblooded) : base(name, age, Iscoldblooded)
+        {
+
+        }
+        public void makesound()
+        {
+            Console.WriteLine("The Snake hiss");
+        }
+        public void display()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Iscoldblooded: " + Iscoldblooded);
+        }
+    }
+    class Eagle:Bird
+    {
+        public Eagle(string name, int age, int wing) : base(name, age, wing)
+        {
+
+        }1
+        public void makesound()
+        {
+            Console.WriteLine("The eagle screeches");
+        }
+        public void display()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("wing: " + wing);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion l = new Lion("Simbha", 3, "Orange");
+            l.display();
+            l.walk();
+            Snake s = new Snake("Kaa", 5, true);
+            s.display();
+            s.Crawl();
+            Eagle e = new Eagle("Eeglu", 2, 4);
+            e.display();
+            e.Fly();
+          
+             Console.ReadKey();
+            }
+        }
+    }
+
---
+++ Csharp_programs/machine_test_bank.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace machine_test_bank
+{
+    class Bank
+    {
+        List<Customer> cust;
+
+        public Bank() 
+        {
+            cust = new List<Customer>();
+            
+        }
+        public void addcustomer(Customer customer)
+        {
+            cust.Add(customer);
+        }
+        public void displaycustomer()
+        {
+            Console.WriteLine(cust);
+        }
+        
+    }
+    class Customer
+    {
+        public string name;
+        
+        List<Account> accounts;
+        public Customer(string name)
+        {
+            accounts = new List<Account>();
+            this.name = name;
+        }
+        public void addaccount(Account account)
+        {
+            accounts.Add(account);
+        }
+        
+    }
+
+    class Account
+    {
+        public int accid;
+        public int bal = 1000;
+        public int amt;
+
+        public Account(int accid,int amt)
+        {
+            this.accid = accid;
+            this.amt = amt;
+        }
+        public void deposit()
+        {
+            bal=bal+amt;
+        }
+        public void withdraw()
+        {
+            bal = bal - amt;
+        }
+        public void Display()
+        {
+
+            Console.WriteLine("Account Details:");
+
+           
+               // Console.WriteLine("Name: {0}", account.name);
+                Console.WriteLine("Account No: {0}", accid);
+                Console.WriteLine("Balance: {0}", bal);
+                Console.WriteLine("__________________________");
+            
+
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            while (true)
+            {
+
+                Console.WriteLine("Enter Name of customer: ");
+                string cust1 = Console.ReadLine();
+                Console.WriteLine("Enter Account no: ");
+                int accno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Amount: ");
+                int amt = Convert.ToInt32(Console.ReadLine());
+                Customer c1 = new Customer(cust1);
+                Account a1 = new Account(accno, amt);
+                Console.WriteLine("Do you want to deposit or withdrawl");
+                string choice = Console.ReadLine();
+                if (choice == "deposit")
+                {
+                    a1.deposit();
+                    a1.Display();
+
+                }
+                else if (choice == "withdrawl")
+                {
+                    a1.withdraw();
+                    a1.Display();
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
