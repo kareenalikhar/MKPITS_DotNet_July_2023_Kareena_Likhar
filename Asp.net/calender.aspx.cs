5fe73657142c3cd70ecc3361d6ffb53f1969a316
Kareena Likhar <kareenalikhar2002@gmail.com>
2024-01-18 17:28:56 +0530

    asp.net

---
+++ Asp.net/calender.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.net3
+{
+    public partial class calender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text=Calendar1.SelectedDate.ToShortDateString();
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label1.Text= Calendar1.SelectedDate.Date.ToShortDateString();
+        }
+    }
+}
